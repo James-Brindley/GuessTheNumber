02e7376071290bab77bced2bbed7fdfdde959068
James Brindley <jamesbrindley8@gmail.com>
2025-09-19 12:36:11 +0100

    Adding a number that makes the player loose

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const randomNumber = Math.floor(Math.random() * 100) + 1;
+const randomBadNumber = Math.floor(Math.random() * 100) + 1;
 
 function checkGuess() {
     let userGuess = parseInt(document.getElementById("userGuess").value);
@@ -8,6 +9,8 @@ function checkGuess() {
         feedback.textContent = "Too low! Try again.";
     } else if (userGuess > randomNumber) {
         feedback.textContent = "Too high! Try again.";
+    } else if (userGuess == randomBadNumber) {
+        feedback.textContent = "You Loose";
     } else {
         feedback.textContent = "Correct! You guessed the number!";
     }

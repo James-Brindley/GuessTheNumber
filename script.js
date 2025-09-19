ff8a800093e9971fde15046dc114ec886f41f3fd
James Brindley <jamesbrindley8@gmail.com>
2025-09-19 12:39:15 +0100

    Fixing random number to be checked before too low

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ function checkGuess() {
     let userGuess = parseInt(document.getElementById("userGuess").value);
     let feedback = document.getElementById("feedback");
 
-    if (userGuess < randomNumber) {
-        feedback.textContent = "Too low! Try again.";
+    if (userGuess == randomBadNumber) {
+        feedback.textContent = "You Loose";
     } else if (userGuess > randomNumber) {
         feedback.textContent = "Too high! Try again.";
-    } else if (userGuess == randomBadNumber) {
-        feedback.textContent = "You Loose";
+    } else if (userGuess < randomNumber) {
+        feedback.textContent = "Too low! Try again.";
     } else {
         feedback.textContent = "Correct! You guessed the number!";
     }

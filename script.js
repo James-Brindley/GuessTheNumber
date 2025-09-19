fc2653f9817082af24a92b3ed558266fedf3fc1c
James Brindley <jamesbrindley8@gmail.com>
2025-09-19 12:58:00 +0100

    adding helth function

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const randomNumber = Math.floor(Math.random() * 100) + 1;
-const randomBadNumber = 9;
+
+let randomBadNumber = Math.floor(Math.random() * 100) + 1;
+while (randomBadNumber === randomNumber) {
+    randomBadNumber = Math.floor(Math.random() * 100) + 1;
+}
+
+let hp = 6;
 
 function checkGuess() {
     let userGuess = parseInt(document.getElementById("userGuess").value);
     let feedback = document.getElementById("feedback");
+    let hpDisplay = document.getElementById("hpDisplay");
 
-    if (userGuess == randomBadNumber) {
-        feedback.textContent = "You Loose";
+    if (userGuess === randomBadNumber) {
+        feedback.textContent = "You Loose a life!";
+        hp -= 1;
+        hpDisplay.textContent = hp;
         return;
     } else if (userGuess > randomNumber) {
         feedback.textContent = "Too high! Try again.";

31c0a68eb814200f4bcbd53b1bc1dbb361d2978d
James Brindley <jamesbrindley8@gmail.com>
2025-10-22 23:40:15 +0100

    more timing fix

--- script.js
+++ script.js
@@ -830,7 +830,7 @@ howToPlayBtn.addEventListener("click", () => {
     mainMenu.style.display = "none";
     mainMenu.classList.remove("menu-fade-out");
     howToPlayScreen.classList.remove("menu-fade-in");
-  }, 600); // matches animation duration
+  }, 400); // matches animation duration
 });
 
 backToMenuBtn.addEventListener("click", () => {
